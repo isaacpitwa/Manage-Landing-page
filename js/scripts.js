5192739ff66435d7ea5ec451202d48da375a7805
Isaac Pitwa <isaacpitwa256@gmail.com>
2020-07-18 21:50:24 +0300

    Courasel set up

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,27 @@
-var slideIndex = 0;
 showSlides();
-
+var startIndex=1;
+var endIndex = (startIndex + 3);
 function showSlides() {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
+  var slides = document.getElementsByClassName("slide");
+//   Hide all the Slides
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
+//   display first 3 slides
+  for (let index = startIndex; index < (startIndex+3); index++) {
+    //   if(startIndex>slides)
+        const slideIndex = index>slides.length? (index-(slides.length-1)):index
+        console.log(`Slide Index:${slideIndex}`)
+      const element = slides[slideIndex];
+      element.style.display = "block"
+  }
+//   Remove first slide and add next slide at the end
+    startIndex++;
+    if (startIndex >= slides.length) {startIndex=1}
+//   slideIndex.shift();
+//   slideIndex[0].style.display = "none"
+//   slides[3].style.display = "block"
+//   slideIndex.push(slides[3]);
   setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
