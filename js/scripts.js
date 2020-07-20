52a60d2fa5f592f6b82ecc58ae5b9c9f687f43f1
Isaac Pitwa <isaacpitwa256@gmail.com>
2020-07-20 10:25:54 +0300

    add White logo

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,29 @@
 showSlides();
 var startIndex=1;
-var endIndex = (startIndex + 3);
+var endIndex = (startIndex + 2);
+var slidesShowing = []
 function showSlides() {
   var i;
   var slides = document.getElementsByClassName("slide");
 //   Hide all the Slides
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
+    if(i>=startIndex){
+      slides[i].style.display = "block";
+      slidesShowing.push(slides[i]);
+      if(i==2 && slidesShowing.length<3){
+        console.log("UPdating Slides")
+        for(var j = 1; (slidesShowing.length+j)<3;j++){
+          slides[0].style.display = "block";
+        }
+
+      }
+     console.log(startIndex)
+    }
   }
-//   display first 3 slides
-  for (let index = startIndex; index < (startIndex+3); index++) {
-    //   if(startIndex>slides)
-        const slideIndex = index>slides.length? (index-(slides.length-1)):index
-        console.log(`Slide Index:${slideIndex}`)
-      const element = slides[slideIndex];
-      element.style.display = "block"
-  }
-//   Remove first slide and add next slide at the end
     startIndex++;
-    if (startIndex >= slides.length) {startIndex=1}
-//   slideIndex.shift();
-//   slideIndex[0].style.display = "none"
-//   slides[3].style.display = "block"
-//   slideIndex.push(slides[3]);
+     if (startIndex > slides.length) {
+       console.log("StartIndex is greater than 3:");
+       startIndex=1; slidesShowing = []}
   setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
