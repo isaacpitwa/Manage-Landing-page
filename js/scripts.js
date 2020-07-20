c11d26586255820bdeb2ec5babdc3f2cef11af63
Isaac Pitwa <isaacpitwa256@gmail.com>
2020-07-20 11:03:56 +0300

    Use Owl-courasel for TEstimonial slider

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,19 @@
-showSlides();
-var startIndex = 1;
-var endIndex = (startIndex + 2);
-var slidesShowing = []
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("slide");
-  //   Hide all the Slides
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-    if (i >= startIndex) {
-      slides[i].style.display = "block";
-      slidesShowing.push(slides[i]);
-      if (i == 2 && slidesShowing.length < 3) {
-        console.log("UPdating Slides")
-        for (var j = 1; (slidesShowing.length + j) < 3; j++) {
-          slides[0].style.display = "block";
-        }
+$(document).ready(function () {
 
+  $(".owl-carousel").owlCarousel({
+    autoPlay: 3000,
+    items: 3,
+    itemsDesktop: [1199, 3],
+    itemsDesktopSmall: [979, 3],
+    center: true,
+    nav: true,
+    loop: true,
+    responsive: {
+      600: {
+        items: 3
       }
-      console.log(startIndex)
     }
-  }
-  startIndex++;
-  if (startIndex > slides.length) {
-    console.log("StartIndex is greater than 3:");
-    startIndex = 1; slidesShowing = []
-  }
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
+
+  });
+
+});
